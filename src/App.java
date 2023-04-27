2e9b652c12f7caf590b5c9a32b6db16315e8e3e8
akemper1589 <akemper@towson.edu>
2023-04-27 18:32:34 -0400

    Put in two new vars

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int x = 0;
+        double y = 15;
     }
 }
